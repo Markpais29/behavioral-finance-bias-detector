40803b61eb06be3308c0468bc0a6dc28f2e32dd8
Mark Pais <itsmarkpais@gmail.com>
2025-07-11 10:06:00 -0400

    data collection script done

---
+++ data_collection.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+
+stocks = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA',
+          'NVDA', 'META', 'JPM', 'BAC', 'NFLX',
+          'SPY']
+
+start_date = '2019-01-01'
+end_date = '2024-07-01'
+
+data = yf.download(stocks, start=start_date, end=end_date)
+
+close = data['Close']
+volume = data['Volume']
+
+close = close.fillna(method='ffill')
+volume = volume.fillna(0)
+
+close.to_csv('close_prices.csv')
+volume.to_csv('volumes.csv')
+
+print("Data downloaded & saved")
